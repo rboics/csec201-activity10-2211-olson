015da90f8aed21d876bea32efa5bbe74b785a860
rboics <28930740+rboics@users.noreply.github.com>
2021-09-08 19:46:58 -0400

    Create myarray.c

---
+++ myarray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+  char chars[] = {'A', 'B', 'C', 'D', '\0'};
+  // 1. Write a for that terates over the chars array and prints out each letter individually
+  // Must include two print statement, one with [] and one without.
+  
+  printf("\n");
+ 
+  // 2. Write a for loop that use a pointer as a control variable to iterate and print each character in the list
+  
+  printf("\n");
+  
+  ptr = chars;
+  // 3. Use a while loop to iterate over the chars array and print each char until we hit the end of the array
+  
+  printf("\n"); 
+ 
+  return 0;
+}
